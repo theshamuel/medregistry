01b8bf7447556f7f892291d17ce1b7875903308f
theshamuel <theshamuel@gmail.com>
2020-04-28 23:31:34 +0100

    add forwarding requests to API V2 in nginx conf

--- web/scripts/entrypoint.sh
+++ web/scripts/entrypoint.sh
@@ -10,13 +10,19 @@ if [ -f /etc/nginx/medregistry.conf ]; then
     cp -fv /etc/nginx/medregistry.conf /etc/nginx/conf.d/medregistry.conf
 fi
 
-echo "PROXY_SERVER=${PROXY_SERVER}, PROXY_PORT=${PROXY_PORT}"
-PROXY_SERVER=${PROXY_SERVER}
-PROXY_PORT=${PROXY_PORT}
-#replace PROXY_SERVER and PROXY_PORT by actual keys
-sed -i "s|PROXY_SERVER|${PROXY_SERVER}|g" /etc/nginx/conf.d/*.conf
-sed -i "s|PROXY_PORT|${PROXY_PORT}|g" /etc/nginx/conf.d/*.conf
+echo "PROXY_SERVER_V1=${PROXY_SERVER_V1}, PROXY_PORT_V1=${PROXY_PORT_V1}"
+echo "PROXY_SERVER_V2=${PROXY_SERVER_V2}, PROXY_PORT_V2=${PROXY_PORT_V2}"
+PROXY_SERVER_V1=${PROXY_SERVER_V1}
+PROXY_PORT_V1=${PROXY_PORT_V1}
+PROXY_SERVER_V2=${PROXY_SERVER_V2}
+PROXY_PORT_V2=${PROXY_PORT_V2}
+#replace PROXY_SERVER_V1 and PROXY_PORT_V1 by actual keys
+sed -i "s|PROXY_SERVER_V1|${PROXY_SERVER_V1}|g" /etc/nginx/conf.d/*.conf
+sed -i "s|PROXY_PORT_V1|${PROXY_PORT_V1}|g" /etc/nginx/conf.d/*.conf
 
+#replace PROXY_SERVER_V2 and PROXY_PORT_V2 by actual keys
+sed -i "s|PROXY_SERVER_V2|${PROXY_SERVER_V2}|g" /etc/nginx/conf.d/*.conf
+sed -i "s|PROXY_PORT_V2|${PROXY_PORT_V2}|g" /etc/nginx/conf.d/*.conf
 (
  sleep 5 #give nginx time to start
  echo "start"
