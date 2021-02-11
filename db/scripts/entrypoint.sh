44a7f1e81a057ce124735c00ba1c3f98f0364a2c
theshamuel <theshamuel@gmail.com>
2021-02-11 23:25:16 +0000

    add fix for formatting etrypoint.sh for DB container

--- db/scripts/entrypoint.sh
+++ db/scripts/entrypoint.sh
@@ -10,18 +10,19 @@ echo "MONGO_ADMIN=${MONGO_ADMIN}, MONGO_ADMIN_PASSWORD=${MONGO_ADMIN_PASSWORD}, 
 echo "MONGO_AUTH=${MONGO_AUTH}"
 
 if [ "${MONGO_AUTH}" = true ]; then
-(mongod --auth) &(
-sleep 5;
-echo "=> Creating ADMIN pwd"
-mongo admin --eval "db.createUser({ user: '$MONGO_ADMIN', pwd: '$MONGO_ADMIN_PASSWORD', roles: [ { role: 'root', db: 'admin' } ] })" && /
-/restore.sh && /
-/create_user.sh &&/
-/backup_script.sh)
+  (mongod --auth) &(
+    sleep 5;
+    echo "=> Creating ADMIN pwd"
+    mongo admin --eval "db.createUser({ user: '$MONGO_ADMIN', pwd: '$MONGO_ADMIN_PASSWORD', roles: [ { role: 'root', db: 'admin' } ] })" && /
+    /restore.sh && /
+    /create_user.sh && /
+    /backup_script.sh
+  )
 else
   (mongod) &(
-  sleep 5;
-  echo "=> Mongo start in test mode without credentials"
-  /restore.sh
+    sleep 5;
+    echo "=> Mongo start in test mode without credentials"
+    /restore.sh
   )
 fi
 fg
