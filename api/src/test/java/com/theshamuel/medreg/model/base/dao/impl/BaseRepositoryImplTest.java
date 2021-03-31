9066bcd53ce1288937e9e6c265fe7fd4f660cb2c
theshamuel <theshamuel@gmail.com>
2021-04-01 00:00:31 +0100

    add refactor BaseRepositoryImplTest

--- api/src/test/java/com/theshamuel/medreg/model/base/dao/impl/BaseRepositoryImplTest.java
+++ api/src/test/java/com/theshamuel/medreg/model/base/dao/impl/BaseRepositoryImplTest.java
@@ -8,7 +8,6 @@ import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
 import java.io.IOException;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
@@ -19,14 +18,13 @@ public abstract class BaseRepositoryImplTest implements BaseRepositoryTest {
     private static final String DB_NAME = "medregDBTest";
     protected static MongoCollection collection;
     protected static MongoTemplate template;
-    private static MongoClient mongo;
     private static MongodForTestsFactory factory = null;
     private static MongoDatabase db;
 
     @BeforeClass
     public static void initDb() throws IOException {
         factory = MongodForTestsFactory.with(Version.Main.PRODUCTION);
-        mongo = factory.newMongo();
+        MongoClient mongo = factory.newMongo();
         db = mongo.getDatabase(DB_NAME);
         template = new MongoTemplate(mongo, DB_NAME);
     }
