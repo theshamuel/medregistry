98a4a425dbba6d94d96407a468af3038d7f62771
theshamuel <theshamuel@gmail.com>
2021-05-30 15:01:36 +0100

    delete redundant method from base class for tests

--- api/src/test/java/com/theshamuel/medreg/base/dao/BaseRepositoryTest.java
+++ api/src/test/java/com/theshamuel/medreg/base/dao/BaseRepositoryTest.java
@@ -3,9 +3,4 @@ package com.theshamuel.medreg.base.dao;
 public interface BaseRepositoryTest {
 
     void createTestRecords();
-
-
-    //Add temporary till upgrade all tests
-    default void setMongo() {
-    }
 }
